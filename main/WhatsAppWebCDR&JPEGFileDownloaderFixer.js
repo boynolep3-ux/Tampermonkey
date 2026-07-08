15468276aeea7a21f83f2ef59aab1a395072831f
boynolep3-ux <boynolep3@gmail.com>
2026-07-08 15:48:28 +0700

    Update WhatsAppWebCDR&JPEGFileDownloaderFixer.js

--- main/WhatsAppWebCDR&JPEGFileDownloaderFixer.js
+++ main/WhatsAppWebCDR&JPEGFileDownloaderFixer.js
@@ -27,19 +27,21 @@
         return newFileName;
     }
 
-    // Mengintersepsi elemen <a> ketika diklik untuk mengunduh
-    document.addEventListener('click', function(event) {
-        const target = event.target.closest('a');
-        if (target && target.hasAttribute('download')) {
-            let fileName = target.getAttribute('download');
-            let fixedName = fixFileName(fileName);
-            if (fixedName !== fileName) {
-                target.setAttribute('download', fixedName);
+    // TEKNIK 1: Mencegat pembuatan Object URL (Akar tautan unduhan blob WhatsApp)
+    const originalCreateObjectURL = URL.createObjectURL;
+    URL.createObjectURL = function(blob) {
+        // Jika objek yang dibuat adalah File dan memiliki properti nama
+        if (blob instanceof File && blob.name) {
+            const fixedName = fixFileName(blob.name);
+            if (fixedName !== blob.name) {
+                // Buat objek File baru dengan nama yang sudah diperbaiki
+                arguments[0] = new File([blob], fixedName, { type: blob.type });
             }
         }
-    }, true);
+        return originalCreateObjectURL.apply(this, arguments);
+    };
 
-    // Mengantisipasi metode unduhan via pemicu klik elemen <a> secara programatis
+    // TEKNIK 2: Mengantisipasi metode unduhan via pemicu klik elemen <a> secara programatis (Tetap dipertahankan)
     const originalAnchorClick = HTMLAnchorElement.prototype.click;
     HTMLAnchorElement.prototype.click = function() {
         if (this.hasAttribute('download')) {
@@ -51,4 +53,17 @@
         }
         originalAnchorClick.apply(this, arguments);
     };
+
+    // TEKNIK 3: Pantau DOM jika WhatsApp menyisipkan elemen unduhan secara dinamis
+    document.addEventListener('pointerdown', function(event) {
+        const target = event.target.closest('a');
+        if (target && target.hasAttribute('download')) {
+            let fileName = target.getAttribute('download');
+            let fixedName = fixFileName(fileName);
+            if (fixedName !== fileName) {
+                target.setAttribute('download', fixedName);
+            }
+        }
+    }, true);
+
 })();
